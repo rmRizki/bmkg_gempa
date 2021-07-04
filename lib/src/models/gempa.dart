d05ec450f4374a4b6d5c1b93cad0e7b85809153a
Rizki M <rzrizkimaulana@gmail.com>
2021-07-05 03:11:29 +0700

    Fix dart formatting

--- lib/src/models/gempa.dart
+++ lib/src/models/gempa.dart
@@ -58,7 +58,9 @@ class Gempa {
   factory Gempa.fromJson(Map<String, dynamic> json) => Gempa(
         tanggal: json['Tanggal'] as String?,
         jam: json['Jam'] as String?,
-        dateTime: json['DateTime'] == null ? null : DateTime.parse(json['DateTime'] as String),
+        dateTime: json['DateTime'] == null
+            ? null
+            : DateTime.parse(json['DateTime'] as String),
         coordinates: json['Coordinates'] as String?,
         lintang: json['Lintang'] as String?,
         bujur: json['Bujur'] as String?,
@@ -67,6 +69,8 @@ class Gempa {
         wilayah: json['Wilayah'] as String?,
         potensi: json['Potensi'] as String?,
         dirasakan: json['Dirasakan'] as String?,
-        shakemap: json['Shakemap'] == null ? null : '$_baseUrl${(json['Shakemap'] as String?)}',
+        shakemap: json['Shakemap'] == null
+            ? null
+            : '$_baseUrl${(json['Shakemap'] as String?)}',
       );
 }
