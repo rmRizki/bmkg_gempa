aea8b42eedb94eedd51058942ee9a328207036cc
Rizki M <rzrizkimaulana@gmail.com>
2021-07-05 03:57:52 +0700

    Remove flutter dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+* Remove flutter dependencies
+
 ## 1.0.1
 
 * Add API Testing
--- test/bmkg_gempa_test.dart
+++ test/bmkg_gempa_test.dart
@@ -1,5 +1,5 @@
 import 'package:bmkg_gempa/bmkg_gempa.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   late BmkgGempa client;
